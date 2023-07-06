1589bea1ba58fbf38c586a4ca8e76ca9d219c957
strunk <astrunk87@gmail.com>
2023-07-05 20:18:43 -0500

    added code from help of instructor

--- assets/script.js
+++ assets/script.js
@@ -10,28 +10,54 @@ var generatePassword = function() {
     var numchoice = window.confirm("include numbers?");
     var specialchoice = window.confirm("include special characters?");
     var lengthchoice = window.prompt("enter length for passward, betwen 8 to 128");
+
+    var specialcharacters = []
     
    
     if (letterchoice) {              
         var index = Math.floor(Math.random() * letters.length);
         console.log(letters[index]);  
-        pass1 = (letters[index]);   
+        specialcharacters.push(letters[index]);   
            
-     }
-     
+     }     
 
     if (numchoice) {
         var index = Math.floor(Math.random() * numbers.length);
         console.log(numbers[index]);
-        pass2 = (numbers[index]); 
+        specialcharacters.push(numbers[index]); 
     }
     if (specialchoice) {
         var index = Math.floor(Math.random() * special.length);
         console.log(special[index]);
-        pass3 = (special[index]);
-        //  a = pass3.repeat(lengthchoice);
-        //  console.log(a);
+        specialcharacters.push(special[index]);
+        
     }
+
+        
+
+    var totalcharacters = (numbers.concat(letters,special));
+    console.log(totalcharacters);
+
+    var temp = []
+
+    for(var i = 0; i<lengthchoice; i++){
+        temp.push(Math.floor(Math.random()*totalcharacters.length));
+        
+    }
+    console.log(temp);
+    temp.splice(-specialcharacters.length);
+    console.log(temp);
+    
+    for(var i = 0; i<specialcharacters.length; i++){
+        temp.push(specialcharacters[i]);
+
+    }
+    console.log(temp);
+    
+    var password = temp.join("");
+
+    console.log(password)
+
 
 
     
@@ -39,24 +65,10 @@ var generatePassword = function() {
         console.log(password);
     } else if(lengthchoice >128 || lengthchoice <8){
         alert("please choose length between 8 and 128")
-        return
-    }  }
-    alert(password);
-    
-    
         
-   
-    
-
-
-}
-// var lengthchoice = window.prompt("enter length betwen 8 to 128");
-    
-
-// var passwordText=computerchoice;
-// console.log(passwordText);
-
-
+    }  
+    alert(password);
+} 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
